5ba6c18a305d1152fa85f762a01928cdb1857d42
sheshnath08 <sheshnathyadav08@gmail.com>
2016-07-13 16:13:00 -0400

    BinaryTree insert Done

--- src/Run/Controller.java
+++ src/Run/Controller.java
@@ -3,6 +3,7 @@ package Run;
 import Applications.StackApplications;
 import BasicDataStructures.LinkedList;
 import BasicDataStructures.Stack;
+import Trees.BinaryTree;
 
 /**
  * Created by sheshnath on 6/1/2016.
@@ -13,10 +14,15 @@ public class Controller {
         String s1 = "12+2-";
         int a[] = {1,3,4,5,6,7};
         int b[] = {1,1};
-        StackApplications sa = new StackApplications();
+       /* StackApplications sa = new StackApplications();
         Misc m = new Misc();
-        System.out.println(m.findMin(a));
+        System.out.println(m.findMin(a));*/
         //System.out.println(sa.kSmallestPairs(a,b,2));
         //System.out.println(sa.evaluatePostFix(s1));
+        BinaryTree bt =  new BinaryTree();
+        bt.insert(1);
+        bt.insert(10);
+        bt.insert(3);
+        bt.insert(0);
     }
 }
--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -6,7 +6,36 @@ package Trees;
 public class BinaryTree {
     Node root;
     public BinaryTree(){
-        root = new Node();
+        root = null;
     }
+    //methdo to insert data onto binary tree
+    public void insert(int data){
+        Node temp = new Node();
+        temp.data = data;
+        if(root == null){
+            root = temp;
+            return;
+        }
+        Node current = root;
+        Node parent = root;
+        while (true){
+            //moving right
+            if(parent.data < data){
+                current = current.right;
+                if(current == null){
+                    parent.right = temp;
+                    return;
+                }
+            }
+            else{
+                current = current.left;
+                if(current == null){
+                    parent.left = temp;
+                    return;
+                }
+            }
+            parent = current;
 
+        }
+    }
 }
